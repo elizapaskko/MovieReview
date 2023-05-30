ca331c16bac2fa2710a7e29fbd9a35b8d8b73410
elizapasko <eliza.pasko@gmail.com>
2023-05-30 17:30:08 +0000

    done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: %i[ show edit update destroy ]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: %i[ index show ]
 
   def index
     @movies = SearchMovieQuery.new
